bf224cee20a36c8ab9c14ad474f23d6991dbea3c
Radhik Bhojani <radhikbhojani1234@gmail.com>
2020-09-08 09:31:14 +0530

    Updates

---
+++ precache-manifest.f7e715d99bdfa6a7a70fce16545717ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25b61c2ad322a3155fccfd3f50346f79",
+    "url": "/Creative/index.html"
+  },
+  {
+    "revision": "798d014cddfaf7c7fb43",
+    "url": "/Creative/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2b8a8631bf4cfd4e0373",
+    "url": "/Creative/static/js/2.c833bf5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Creative/static/js/2.c833bf5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "798d014cddfaf7c7fb43",
+    "url": "/Creative/static/js/main.5169f597.chunk.js"
+  },
+  {
+    "revision": "d76ca5d0c2d19004e2c0",
+    "url": "/Creative/static/js/runtime-main.664334fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Creative/static/media/logo.5d5d9eef.svg"
+  }
+]);
